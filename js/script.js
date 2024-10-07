43fc4708b369445206cd2b22b325df6615835194
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-10-07 20:09:48 +0200

    Co-authored-by: José Molina  <Jmolmel@users.noreply.github.com> Co-authored-by: Raquel López Bermúdez <Rlopber@users.noreply.github.com> Co-authored-by: Pablo Ruiz <pablo-r-d@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -3,36 +3,49 @@
 // Cargar DOM
 const botonVaciarCarrito = document.querySelector("#vaciar-carrito"); // Boton vaciar carrito
 const listaCursos = document.querySelector("#lista-cursos"); // Array de todos los cursos total 12  //.container
-const carrito = document.querySelector("#carrito") // El contenedor del carrito
+const carrito = document.querySelector("#lista-carrito") // El contenedor del carrito
 const botonAgregarCarrito = document.querySelectorAll(".agregar-carrito");  // Está dentro del <a> en class </a>
+const cantidades = new Map();
+
 
 // Variables
 
-const objetos = []; // Array de Objetos 
+const curso = [];
+let carritoProductos = [];
+
 
 // Eventos
 
 listaCursos.addEventListener('click', agregarCurso);
-
 
 // botonVaciarCarrito.addEventListener("click", vaciarCarrito());
 
 // Funciones
 
 /**
- * Funcion para agregar curso al carrito.
+ * Funcion para recoger datos al hacer click en el boton "agregar carrito"
  */
-function agregarCurso(event) {
+function recogerDatosCurso(event) {
   if (event.target.classList.contains("agregar-carrito")) {
-    const cursoSeleccionado = event.target.parentElement.parentElement;
-    const datosProducto = {
-        imagenCurso: cursoSeleccionado.children[0].src,
-        nombreCurso: cursoSeleccionado.children[1].children[0].textContent,
-        precioCurso: cursoSeleccionado.children[1].children[3].children[0].textContent
+    let cursoSeleccionado = event.target.parentElement.parentElement;
+    curso = {
+      id: cursoSeleccionado.children[1].children[4].getAttribute("data-id"),
+      imagenCurso: cursoSeleccionado.children[0].src,
+      nombreCurso: cursoSeleccionado.children[1].children[0].textContent,
+      precioCurso: cursoSeleccionado.children[1].children[3].children[0].textContent,
+      contador: 0
     }
-    
-    console.log(datosProducto);
+
+    // setcarritoProducto(datosProducto);
+    // crearTrCarrito(datosProducto);
+    console.log(curso);
   }
+}
+
+function setcarritoProducto(curso) {
+  carritoProducto.push(curso);
+  localStorage.setItem("productos", JSON.stringify(carritoProductos))
+
 }
 
 /**
@@ -43,13 +56,18 @@ function vaciarCarrito() {
 }
 
 /**
- * funcion para capturar un objeto
+ * funcion para crear un tr para el carrito
  */
-function capturarObjetos() {
+function crearTrCarrito(producto) {
 
 }
 
 
-
+/**
+ * Funcion para cuando recargues la página se cargue el LocalStorage
+ */
+function actualizarLocalStorage() {
+  JSON.parse(localStorage.getItem("productos"))
+}
 
 
