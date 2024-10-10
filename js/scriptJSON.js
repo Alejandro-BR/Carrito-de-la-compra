53837bc24100c78cb323dbca8dd1000bae089aca
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-10-10 18:33:22 +0200

    sriptJSON

--- js/scriptJSON.js
+++ js/scriptJSON.js
@@ -7,21 +7,26 @@
  */
 
 // Cargar DOM
-const botonVaciarCarrito = document.querySelector("#vaciar-carrito"); // Boton vaciar carrito
-const listaCursos = document.querySelector("#lista-cursos"); // Array de todos los cursos total 12  //.container
-const carrito = document.querySelector("#lista-carrito") // El contenedor del carrito
-const botonAgregarCarrito = document.querySelectorAll(".agregar-carrito");  // Está dentro del <a> en class </a>
+const botonVaciarCarrito = document.querySelector("#vaciar-carrito");
+const listaCursos = document.querySelector("#lista-cursos");
+const carrito = document.querySelector("#lista-carrito");
+const botonAgregarCarrito = document.querySelectorAll(".agregar-carrito");
 const table = document.getElementById('lista-carrito');
 
 // Variables
-let curso = {}; // Objeto curso
-let carritoProductos = JSON.parse(localStorage.getItem("carrito")) || [];; // Array con todos los cursos
+let curso = {};
+let carritoProductos;
 
-// Actualiza la pagina cuando entramos
-crearTrCarrito();
+async function init() {
+  actualizarVariableConLocalStorage();
+  const data = await getCatalogo();
+  mostrarCard(data);
+  crearHtmlCarrito();
+}
+
+init();
 
 // Eventos
-
 listaCursos.addEventListener('click', recogerDatosCurso);
 botonVaciarCarrito.addEventListener('click', vaciarCarrito);
 table.addEventListener('click', borrarCurso);
@@ -39,7 +44,7 @@ function recogerDatosCurso(event) {
       id: cursoSeleccionado.children[1].children[4].getAttribute("data-id"),
       imagenCurso: cursoSeleccionado.children[0].src,
       nombreCurso: cursoSeleccionado.children[1].children[0].textContent,
-      precioCurso: parseInt(cursoSeleccionado.children[1].children[3].children[0].textContent.replace("$", "")), // Quitar primera posicion
+      precioCurso: parseInt(cursoSeleccionado.children[1].children[3].children[0].textContent.replace("$", "")),
       cantidad: 1
     }
     setcarritoProducto(curso);
@@ -47,7 +52,7 @@ function recogerDatosCurso(event) {
 }
 
 /**
- * Funcion para editar el carrito.
+ * Funcion para agregar un producto al carrito
  * 
  * @param {*} curso 
  */
@@ -67,7 +72,7 @@ function setcarritoProducto(curso) {
   }
 
   actualizarLocalStorage();
-  crearTrCarrito();
+  crearHtmlCarrito();
 }
 
 /**
@@ -80,48 +85,33 @@ function setcarritoProducto(curso) {
 function vaciarCarrito() {
   carritoProductos = [];
   localStorage.removeItem("carrito");
-
-  crearTrCarrito();
+  crearHtmlCarrito();
 }
 
 /**
  * Funcion para crear un tr para el carrito
  */
-function crearTrCarrito() {
+function crearHtmlCarrito() {
 
   actualizarVariableConLocalStorage();
-  table.children[1].innerHTML = "";
+  const tbody = table.querySelector("tbody");
+  tbody.innerHTML = "";
 
-  carritoProductos.forEach(c => {
+  carritoProductos.forEach(curso => {
     const tr = document.createElement('tr');
 
-    let th1 = document.createElement('th');
-    let img = document.createElement('img');
-    img.setAttribute('src', c.imagenCurso);
-    th1.appendChild(img);
-    tr.appendChild(th1);
+    tr.innerHTML = `
+      <th><img src="${curso.imagenCurso}" width="100"></th>
+      <th>${curso.nombreCurso}</th>
+      <th>$${curso.precioCurso}</th>
+      <th>${curso.cantidad}</th>
+      <th><a href="#" class="borrar-curso" id="${curso.id}">X</a></th>
+    `;
 
-    let th2 = document.createElement('th');
-    th2.textContent = c.nombreCurso;
-    tr.appendChild(th2);
-
-    let th3 = document.createElement('th');
-    th3.textContent = "$" + c.precioCurso;
-    tr.appendChild(th3);
-
-    let th4 = document.createElement('th');
-    th4.textContent = c.cantidad;
-    tr.appendChild(th4);
-
-    // Crear el enlace para borrar el tweet
-    const th5 = document.createElement("th");
-    th5.innerHTML = `<a class="borrar-curso" id=${c.id}>X</a>`
-    tr.appendChild(th5);
-
-
-    table.children[1].appendChild(tr);
+    tbody.appendChild(tr);
   })
 }
+
 
 /**
  * Función para borrar un curso.
@@ -129,12 +119,10 @@ function crearTrCarrito() {
  * @param {*} event 
  */
 function borrarCurso(event) {
-  console.log(event.target);
-
   if (event.target.classList.contains("borrar-curso")) {
     carritoProductos = carritoProductos.filter(c => c.id !== event.target.id);
     actualizarLocalStorage();
-    crearTrCarrito();
+    crearHtmlCarrito();
 
     if (carritoProductos.length === 0) {
       localStorage.removeItem("carrito");
@@ -156,8 +144,94 @@ function actualizarLocalStorage() {
   localStorage.setItem("carrito", JSON.stringify(carritoProductos));
 }
 
-
+/**
+ * Obtener el json
+ * 
+ * @returns data o error
+ */
 async function getCatalogo() {
-  let response = await ('data/data.json');
+  let response;
+  try {
+    // Usa fetch para obtener el archivo JSON
+    response = await fetch('data/data.json');
+    // Convierte la respuesta a JSON
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error al obtener el catálogo:', error);
+  }
   return response;
+}
+
+/**
+ * Crear el html
+ * 
+ * @param {Array} data 
+ */
+function mostrarCard(data) {
+
+  // for (let i = 0; i < data.length; i += 3) {
+  //   const div = document.createElement('div');
+  //   div.classList.add('row');
+
+  //   data.forEach(c => {
+  //     const div = document.createElement('div');
+  //     div.classList.add('row');
+  //     div.innerHTML = `
+  //         <div class="four columns">
+  //           <div class="card">
+  //               <img src="${c.url}" class="imagen-curso u-full-width" />
+  //               <div class="info-card">
+  //                 <h4>${c.nombre}</h4>
+  //                 <p>${c.autor}$</p>
+  //                 <img src="img/estrellas.png" />
+  //                 <p class="precio">
+  //                   $200
+  //                   <span class="u-pull-right">${c.precio}</span>
+  //                 </p>
+  //                 <a
+  //                   href="#"
+  //                   class="u-full-width button-primary button input agregar-carrito"
+  //                   data-id="${c.id}"
+  //                   >Agregar Al Carrito</a
+  //                 >
+  //               </div>
+  //             </div>
+  //         </div>
+  //     `;
+  //     listaCursos.appendChild(div);
+  //   });
+
+  // }
+  for (let i = 0; i < data.length; i += 3) {
+    const divRow = document.createElement('div');
+    divRow.classList.add('row');
+
+    for (let j = 0; j < data.length; j++) {
+      const div = document.createElement('div');
+      div.classList.add('four columns');
+      div.innerHTML = `
+                <div class="card">
+                    <img src="${data[j].url}" class="imagen-curso u-full-width" />
+                    <div class="info-card">
+                      <h4>${data[j].nombre}</h4>
+                      <p>${data[j].autor}$</p>
+                      <img src="img/estrellas.png" />
+                      <p class="precio">
+                        $200
+                        <span class="u-pull-right">${data[j].precio}</span>
+                      </p>
+                      <a
+                        href="#"
+                        class="u-full-width button-primary button input agregar-carrito"
+                        data-id="${data[j].id}"
+                        >Agregar Al Carrito</a
+                      >
+                    </div>
+                  </div>
+          `;
+    }
+    divRow.appendChild(div);
+  }
+  listaCursos.appendChild(divRow);
 }
